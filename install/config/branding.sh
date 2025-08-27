0ac242b7514bdb0c8ebc45a0d0be703da41d57e2
pinionless <46654199+pinionless@users.noreply.github.com>
2025-08-27 19:06:08 +0200

    chore(branding): update project branding assets and boot splash
    Update ASCII art in boot.sh, logo-alt.txt, and logo.txt.
    Add new logo-pinarchy.txt.
    Update Plymouth boot logo.
    Configure screensaver to use logo-alt.txt.

--- install/config/branding.sh
+++ install/config/branding.sh
@@ -3,4 +3,4 @@
 # Allow the user to change the branding for fastfetch and screensaver
 mkdir -p ~/.config/omarchy/branding
 cp ~/.local/share/omarchy/icon.txt ~/.config/omarchy/branding/about.txt
-cp ~/.local/share/omarchy/logo.txt ~/.config/omarchy/branding/screensaver.txt
+cp ~/.local/share/omarchy/logo-alt.txt ~/.config/omarchy/branding/screensaver.txt
